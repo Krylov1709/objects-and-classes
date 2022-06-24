60be462647a450e847976dde96088662af5da2d2
Крылов Алексей <krylov1709@yandex.ru>
2022-06-25 00:34:04 +0300

    Задание 2

--- objects and classes.py
+++ objects and classes.py
@@ -6,13 +6,27 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+    def rate_lecture(self, lecture, course, grade):
+        if isinstance(lecture, Lecturer) and course in lecture.courses_attached and self.finished_courses:
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[course] = [grade]
+        else:
+            return 'Ошибка'        
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,12 +35,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
